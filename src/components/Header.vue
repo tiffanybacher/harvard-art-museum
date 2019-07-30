a83bf337ea690bb9c3a115a33ab4e73b9b74173a
tiffanybacher <tiffanybacher@gmail.com>
2019-07-30 16:34:13 -0600

    Create Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  padding: 2%;
+
+  img {
+    height: 150px;
+  }
+}
+
+@media screen and (max-width: 320px) {
+  .header {
+    img {
+      height: 75px;
+    }
+  }
+}
+</style>
