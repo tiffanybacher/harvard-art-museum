0a9d9853419155859bbb2b95a925b711ab1f20d7
tiffanybacher <tiffanybacher@gmail.com>
2019-07-30 16:34:31 -0600

    Create Main component

---
+++ src/components/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="main">
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  created: async function() {
+    console.log('testing');
+
+    const url = `https://api.harvardartmuseums.org/image?apikey=${process.env.API_KEY}`;
+
+    await fetch(url)
+      .then(response => response.json())
+      .then(result => console.log(result))
+  }
+};
+</script>
+
+<style lang="scss">
+.main {
+
+}
+</style>
